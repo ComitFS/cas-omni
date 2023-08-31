5b4989e4c7c998766ad4af9812fa4da44b18fb74
Dele Olajide <dele@4ng.net>
2023-08-31 12:39:13 +0100

    dont expose acs

--- demo/client/index.js
+++ demo/client/index.js
@@ -13,13 +13,19 @@ window.addEventListener("load", async function()
 	
 	const destination = { microsoftTeamsUserId: '83ec482c-3bc5-4116-acee-e081cc720630'};
 	const userId = { communicationUserId: '8:acs:7278b90e-91bb-4a42-8913-9233b5d4ad4f_0000001a-e206-aa5f-0cf9-9c3a0d002bf8' };
+
+	const url = urlParam("u") ? urlParam("u") : "";
+	let casUrl = url + "/teams/api/openlink/config/global";	
 	
-	const acs_endpoint = 'endpoint=https://smartcall-acs.communication.azure.com/;accesskey=XkUIJFy58CNmLxWnsXK4q2t7oMlMvivOlTwurKoiPVp5TSdHFqW3jIV7Ee/EJZCPxCV91ZcPfXQBnQ22JBRaaA==';
-	const client = new ACS.CommunicationIdentityClient(acs_endpoint);	
+	let response = await fetch(casUrl, {method: "GET"});
+	const config = await response.json();				
+	const client = new ACS.CommunicationIdentityClient(config.acs_endpoint);	
 	const response2 = await client.getToken(userId, ["chat", "voip"]);		
 	const token = response2.token;	
 	
-	const resp1 = await fetch("/teams/api/openlink/shared/meeting/cas-omni-jjgartland", {method: "GET", headers: {authorization: "1234567890"}});	
+	const authorization = urlParam("t") ? urlParam("t") : "1234567890";
+
+	const resp1 = await fetch(url + "/teams/api/openlink/shared/meeting/cas-omni-jjgartland", {method: "GET", headers: {authorization}});	
 	const line = await resp1.json();	
 	const locator = {meetingLink: line.joinWebUrl};	
 	const callAdapter = await callComposite.loadCallComposite({displayName: "JJ Gartland", locator,	userId,	token}, content, {formFactor: 'mobile',	key: new Date()	});	
