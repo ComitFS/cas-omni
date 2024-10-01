e68ca4de71671eb3c6f187e83971b1401601eb95
Dele Olajide <dele@4ng.net>
2024-10-01 17:34:22 +0100

    fix origin

--- demo/tab/client/index.js
+++ demo/tab/client/index.js
@@ -25,16 +25,19 @@ window.onload = async () => {
 		destination = data.destination;
 		
 	} else {
+		let origin = urlParam("origin");	
+		if (!origin) origin = "http://localhost"
+			
 		const userId = { communicationUserId: '8:acs:7278b90e-91bb-4a42-8913-9233b5d4ad4f_0000000d-ce88-0aa9-0cf9-9c3a0d004831' };
 		const authorization = btoa("jjgartland:Welcome123");
-		const url = location.protocol + "//" + location.host + "/plugins/casapi/v1/companion/config/global";			
+		const url = origin + "/plugins/casapi/v1/companion/config/global";			
 		const response = await fetch(url, {method: "GET", headers: {authorization}});
 		const config = await response.json();				
 		const client = new ACS.CommunicationIdentityClient(config.acs_endpoint);	
 		const response2 = await client.getToken(userId, ["chat", "voip"]);		
 		const token = response2.token;	
 		
-		const url2 = location.protocol + "//" + location.host + "/plugins/casapi/v1/companion/meeting/adviser/dele?subject=JJ Gartland Calling FA";			
+		const url2 = origin + "/plugins/casapi/v1/companion/meeting/adviser/dele?subject=JJ Gartland Calling FA";			
 		const response3 = await fetch(url2, {method: "POST", headers: {authorization}});
 		const meetingLink = await response3.text();		
 		const locator = {meetingLink};
